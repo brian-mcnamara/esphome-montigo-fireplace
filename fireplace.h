5021c9f49e7ad16a9f12a491524390ea0c0d18a4
Brian McNamara <brianmay27@gmail.com>
2024-02-20 18:34:57 -0800

    Fixing the parser

--- fireplace.h
+++ fireplace.h
@@ -15,53 +15,57 @@ using namespace std;
 int getCode(vector<int> transmission) {
 
     boolean found = false;
-    short offset = 116;
     short dataCounter = 0;
-    short counter = 0;
     short blockCounter = 0;
-    unsigned char data[3];
+    short packetSize = 22;
+    unsigned char data[packetSize];
+    unsigned char tempData[packetSize];
     unsigned char block = 0;
 
     for (const int& duration : transmission) {
         //Skip till second transmission
-        if (duration > -4800 && !found) {
-            found = true;
-            continue;
-        } else if (duration < -4800) {
-            break;
-        }
-
-        int bits = round(duration / 413.0);
-        for (int i = 0; i < abs(bits); i++) {
-            if (counter++ < offset) {
+        if (duration < -4800) {
+            if(memcmp(&data, &tempData, sizeof(tempData)) == 0) {
+                found = true;
+                break;
+            } else {
+                ESP_LOGD("Fireplace", "Blocks do not match");
                 continue;
             }
+        }
+
+        int bits = round(duration / 400.0);
+        for (int i = 0; i < abs(bits); i++) {
             int pos = 7 - blockCounter++;
             block |= (bits > 0 ? 1 : 0) << pos;
             if (pos == 0) {
-                data[dataCounter++] = block;
+                tempData[dataCounter++] = block;
                 block = 0;
                 blockCounter = 0;
-                if (dataCounter >= 3) {
-                    ESP_LOGD("Fireplace", "Block received: %02x%02x%02x", data[0], data[1], data[2]);
-                    if ((data[2] & 0x0F) == 0x0a) {
-                        return 0;
-                    } else if (data[0] == 0xb3) {
-                        return 1;
-                    } else if (data[0] == 0xcb) {
-                        return 2;
-                    } else if (data[0] == 0xd2) {
-                        return 3;
-                    } else if (data[0] == 0x95) {
-                        return 4;
-                    } else if (data[0] == 0x32) {
-                        return 5;
-                    } else if (data[0] == 0xa5 || (data[2] & 0x0F) == 0x04) {
-                        return 6;
-                    }
-                    return -1;
-                }
             }
+        }
+        if (dataCounter >= packetSize) {
+            dataCounter = 0;
+            memcpy(data, tempData, packetSize);
+        }
+    }
+
+    if (found) {
+        ESP_LOGD("Fireplace", "Block received: %02x%02x%02x%02x%02x%02x%02x%02x", data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21]);
+        if (data[18] == 0xa5) {
+            return 0;
+        } else if (data[20] == 0x56) {
+            return 1;
+        } else if (data[20] == 0x6a) {
+            return 2;
+        } else if (data[20] == 0x66) {
+            return 3;
+        } else if (data[20] == 0x9a) {
+            return 4;
+        } else if (data[20] == 0x96) {
+            return 5;
+        } else if (data[20] == 0xaa) {
+            return 6;
         }
     }
     ESP_LOGD("Fireplace", "Unknown code sent");
